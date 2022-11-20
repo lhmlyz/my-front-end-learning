ac06575ece0809cfcd406ec1fb036a465b8e8d90
ielyazov <ielyazov2@gmail.com>
2022-11-21 00:42:37 +0400

    numbers, operators, variables

---
+++ javascript/day-1/app.js
@@ -0,0 +1,35 @@
+// primitive types
+// Number => one number type for +/-, whole and decimal
+// String
+// Boolean
+// Null
+// Undefined
+
+// operators / + - % * **
+
+// NaN - not a number,
+
+// 1/0 => infinity
+// -0
+// 0/0 => NaN
+// 1+ NaN => NaN
+
+// variables
+// let
+// const
+
+let age = 10;
+// let age = 10; not allowed
+
+let score = 10;
+score = score + 10; //the same
+score += 10; // the same
+// +=, -=, *=, /=
+// ++ increment by one
+// -- decrement by one
+
+// const => final
+const year = 1998;
+// year = 1998; not allowed
+
+// var => not recommended 
